b324e4eb3189ab22597704546de99e09b7472186
Daisy Yang <3156480637@qq.com>
2019-02-13 21:33:44 +0800

    events

---
+++ js/events/album.js
@@ -0,0 +1,23 @@
+var render = function () {
+    var event_template = $('#event_template').html();
+    Mustache.parse(event_template);
+    $.each(data.album, function () {
+        $('#events_photo').append(Mustache.render(event_template, this));
+    });
+};
+
+$(function () {
+    // admin
+    var storage = window.localStorage;
+    if (storage.getItem('username') === 'admin') {
+        $('.address_book_click').css('display', 'block')
+    }
+
+    var username = storage.getItem('username')
+    $(".btns-language").text(username);
+
+    $('.logout').click(function () {
+        window.localStorage.setItem('username', '')
+        window.location.href = "http://tfire.net/index.html"
+    })
+})
