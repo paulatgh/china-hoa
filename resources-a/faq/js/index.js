ce7da1e228a95ccb08b11e96e85320625a041536
Tom_lujihang <563428704@qq.com>
2019-02-18 11:57:04 +0800

    faq

---
+++ resources-a/faq/js/index.js
@@ -0,0 +1,38 @@
+var render = function () {
+    var Logo = $('#Logo').html();
+    $.each(data.Logo, function () {
+        $('#Logo_cycle').append(Mustache.render(Logo, this));
+    });
+    var event_template = $('#FAQcontent').html();
+    Mustache.parse(event_template);
+    $.each(data.FrequentlyAskedQuestions, function () {
+        $('#FAQcontent_cycle').append(Mustache.render(event_template, this));
+    });
+
+    var event_template = $('#FAQtitle').html();
+    $.each(data.FAQtitle, function () {
+        $('#FAQtitle_cycle').append(Mustache.render(event_template, this));
+    });
+
+
+    // Global post render
+    _post_render();
+
+    // Local post render
+
+    
+
+    // admin
+    var storage = window.localStorage;
+    if (storage.getItem('username') === 'admin') {
+        $('.buttona').css('display', 'none')
+    }
+
+    var username = storage.getItem('username')
+    $(".btns-language").text(username);
+
+    $('.logout').click(function () {
+        window.localStorage.setItem('username', '')
+        window.location.href = "http://tfire.net/index.html"
+    })
+};
