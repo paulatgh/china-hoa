b0a48bc664c8c9eca34012dcfdd8e7a68a6b2bb2
Laurie Williams <laurie.huw.williams@googlemail.com>
2019-02-25 12:51:33 +0700

    photo pages fixes

---
+++ activities/photo/album/add/js/index.js
@@ -0,0 +1,16 @@
+var render = function() {
+    _pre_render();
+    var event_template = $('#add').html();
+    Mustache.parse(event_template);
+    $('#add').after(Mustache.render(event_template, data));
+
+    // Global post render
+    _post_render();
+
+    // Local post render
+    // admin
+    if (data._current_user && data._current_user.is_admin == true) {
+        $('.buttona').css('display', 'none')
+    }
+
+};
