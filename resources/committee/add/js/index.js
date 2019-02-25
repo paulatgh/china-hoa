c34fb4a654ac40a0333625b8d99eaf0c21598f84
Tom_lujihang <563428704@qq.com>
2019-02-25 18:41:55 +0800

    resources/committee/add

--- resources/committee/add/js/index.js
+++ resources/committee/add/js/index.js
@@ -1,19 +1,15 @@
 var render = function() {
     _pre_render()
 
-    // var CommitteeAdd = $('#CommitteeAdd').html();
-    // Mustache.parse(CommitteeAdd);
-    // $('#CommitteeAdd').after(Mustache.render(CommitteeAdd, data.the_title[0]));
-
-    var form = $('#form').html();
-    $('#form').after(Mustache.render(form, data));
+    var table_header = $('#table_header').html()
+    $.each(data.table_header, function() {
+        $('#table_header_cycle').append(Mustache.render(table_header, this));
+    });
 
     // Global post render
     _post_render();
 
     // Local post render
-
-   
 
     if (data._current_user && data._current_user.is_admin == true) {
         $('.buttona').css('display', 'none')
