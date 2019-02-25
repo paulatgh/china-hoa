c38ba6bc772f02fc776813fe546ae312f332c601
Tom_lujihang <563428704@qq.com>
2019-02-25 20:46:46 +0800

    news/announcements/edit

--- news/announcements/edit/js/index.js
+++ news/announcements/edit/js/index.js
@@ -1,8 +1,19 @@
 var render = function() {
-    var content_template = $('#edit').html();
-    Mustache.parse(content_template);
-    $('#edit').after(Mustache.render(content_template, data));
     _pre_render();
+
+    // var content_template = $('#edit').html();
+    // Mustache.parse(content_template);
+    // $('#edit').after(Mustache.render(content_template, data));
+
+    var table_header = $('#table_header').html()
+    $.each(data.table_header, function() {
+        $('#table_header_cycle').append(Mustache.render(table_header, this));
+    });
+
+    var drop_down_list = $('#drop_down_list').html()
+    $.each(data.option, function() {
+        $('#drop_down_list_cycle').append(Mustache.render(drop_down_list, this));
+    });
 
     // Global post render
     _post_render();
