f3bd5f52f0d685472dd7be05b5067e0f756545fc
Calvin Tuong <calvin.tuong@gmail.com>
2019-02-26 21:38:04 -0800

    Update calendar add form

--- activities/calendar/add/js/index.js
+++ activities/calendar/add/js/index.js
@@ -2,6 +2,10 @@ var render = function() {
     _pre_render();
     $("#breadcrumbs").after(function() { return Mustache.render($(this).html(), data); });
 
+    $('#cancel-button').click(function() {
+        window.location = `${data._metadata.root_url}/activities/calendar`;
+    })
+
     // Global post render
     _post_render();
 
