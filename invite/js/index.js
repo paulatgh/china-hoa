06387c4b27e9ad865b34bb55d53cea2ff4c5cfef
Laurie Williams <laurie.huw.williams@googlemail.com>
2019-02-28 13:28:46 +0700

    add invite page

---
+++ invite/js/index.js
@@ -0,0 +1,26 @@
+var render = function() {
+    _pre_render()
+
+    $('#header').after(function() { return Mustache.render($(this).html(), data); });
+    $('#form').after(function() { return Mustache.render($(this).html(), data); });
+   
+    $('#cancel-button').click(function() {
+        window.location = `${data._metadata.root_url}/home`;
+    })
+    // Global post render
+    _post_render();
+
+    // Local post render
+
+    // admin
+    if (data._current_user && data._current_user.is_admin == true) {
+        $('.buttona').css('display', 'none')
+    }
+    var username = data._current_user && data._current_user.display_name
+    $(".btns-language").text(username);
+    $('.logout').click(function() {
+        //TODO: log out user
+        logOutUser()
+    })
+    addAuthenticityTokenToForms();
+}
