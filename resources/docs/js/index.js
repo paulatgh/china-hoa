1aaca8f789cc1eca8ff34c9de7c2178464bde4d5
Calvin Tuong <calvin.tuong@gmail.com>
2019-03-01 16:16:50 -0800

    Add admin actions to docs page

--- resources/docs/js/index.js
+++ resources/docs/js/index.js
@@ -1,36 +1,18 @@
 var render = function () {
     _pre_render()
 
-    var HomeownersAssociation = $('#HomeownersAssociation').html();
-    Mustache.parse(HomeownersAssociation);
-    $.each(data.homeowners_association, function () {
-    $('#HomeownersAssociationExternal').append(Mustache.render(HomeownersAssociation, this)); });
-    var HomeownersAssociation = $('#HomeownersAssociation').html();
-    Mustache.parse(HomeownersAssociation);
-    $.each(data.homeowners_association_second, function () {
-    $('#HomeownersAssociationSecondExternal').append(Mustache.render(HomeownersAssociation, this));});
-    $('#category_template').after(function() { return Mustache.render($(this).html(), data); });
     $('#documents_template').after(function() { return Mustache.render($(this).html(), data); });
-    var categoryName = $('#categoryName').html();
-    Mustache.parse(categoryName);
-    $.each(data.category_name, function () {
-    $('#categoryNameExternal').append(Mustache.render(categoryName, this));});
-    var PDFcategoryList = $('#PDFcategoryList').html();
-    Mustache.parse(PDFcategoryList);
-    $.each(data.pdf_category, function () {
-    $('#PDFcategoryListExternal').append(Mustache.render(PDFcategoryList, this));});
     $("#breadcrumbs").after(function() { return Mustache.render($(this).html(), data); });
+    $("#actions_template").after(function() { return Mustache.render($(this).html(), data); });
 
     // Global post render
     _post_render();
 
     // Local post render
 
-
     // admin
     if (data._current_user && data._current_user.is_admin == true) {
-        $('.announcements_add').css('display', 'block')
-        $('.announcements_permission').css('display', 'block')
+        $('.admin_actions').css('display', 'block')
     }
 
     // $('.tabs a').click(function (e) {
