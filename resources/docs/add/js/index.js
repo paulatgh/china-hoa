1aa5de0b2f39d2d92c9878ee4ccaa30dee907be6
Calvin Tuong <calvin.tuong@gmail.com>
2019-03-02 03:05:10 -0800

    Add uploading docs

--- resources/docs/add/js/index.js
+++ resources/docs/add/js/index.js
@@ -1,19 +1,14 @@
 var render = function() {
     _pre_render();
-    var event_template = $('#add').html();
-    Mustache.parse(event_template);
     $("#breadcrumbs").after(function() { return Mustache.render($(this).html(), data); });
     $('#cancel-button').click(function() {
-        window.location = `${data._metadata.root_url}/activities/photo`;
+        window.location = `${data._metadata.root_url}/resources/docs`;
     })
+
     // Global post render
     _post_render();
 
     // Local post render
-    // admin
-    if (data._current_user && data._current_user.is_admin == true) {
-        $('.buttona').css('display', 'none')
-    }
 
     addAuthenticityTokenToForms();
 };
