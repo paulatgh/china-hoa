cf7ec1442f58fc699713c16358455e216cb7d56d
Calvin Tuong <calvin.tuong@gmail.com>
2019-03-03 10:19:28 -0800

    Add user profile information to address book

--- resources/address/js/index.js
+++ resources/address/js/index.js
@@ -6,13 +6,13 @@ var render = function () {
     var user_information = $('#user_information').html();
 
     Mustache.parse(event_template);
+    Mustache.parse(user_information);
 
     var user_list_section_template = $('#user_list_section_template').html(); $.each(data.names_by_letter, function() { $('#user_list').append(Mustache.render(user_list_section_template, this)); });
-    Mustache.parse(according_to_the_street);$.each(data.according_to_the_street, function () { $('#according_to_the_street_cycle').append(Mustache.render(according_to_the_street, this)); });
+    Mustache.parse(according_to_the_street); $.each(data.according_to_the_street, function () { $('#according_to_the_street_cycle').append(Mustache.render(according_to_the_street, this)); });
     var alphabet = $('#alphabet').html(); $.each(data.letter_anchors, function () { $('#alphabet_cycle').append(Mustache.render(alphabet, this)); });
     var page_information = $('#page_information').html(); $.each(data.page_information, function () { $('#page_information_cycle').append(Mustache.render(page_information, this));});
     var function_button = $('#function_button').html(); $.each(data.function_button, function () { $('#function_button_cycle').append(Mustache.render(function_button, this)); });
-    var user_information = $('#user_information').html(); $.each(data.user_information, function () { $('#user_information_cycle').append(Mustache.render(user_information, this)); });
     var category = $('#category').html(); $.each(data.address_book_categories, function () { $('#address').append(Mustache.render(category, this));});
     $("#breadcrumbs").after(function() { return Mustache.render($(this).html(), data); });
 
@@ -27,8 +27,6 @@ var render = function () {
         $('.announcements_add').css('display', 'block')
         $('.announcements_permission').css('display', 'block')
     }
-
-
 
     var show = true;
     $('.address_book_click').click(function () {
@@ -49,6 +47,11 @@ var render = function () {
     })
 
     $('.user_desc, .a_z .user_desc').click(function () {
+        var userId = this.getAttribute('data-id');
+        var userData = data.user_data.find(function(e) {
+            return e['id'] == userId
+        });
+        $('#user_information_cycle').html(Mustache.render(user_information, userData));
         $(".db").css("display", "block")
         $('.close').click(function () {
             $('.db').css("display", "none")
