3000debe1edbc6b7fc068bfe4f7c1ce62b40e9bb
Laurie Williams <laurie.huw.williams@googlemail.com>
2019-03-16 10:46:15 +0800

    create pages for create committee members

---
+++ resources/committee/members/js/index.js
@@ -0,0 +1,21 @@
+var render = function() {
+    _pre_render()
+
+    var table_header = $('#table_header').html()
+    $.each(data.table_header, function() { $('#table_header_cycle').append(Mustache.render(table_header, this));});
+    $("#breadcrumbs").after(function() { return Mustache.render($(this).html(), data); });
+    
+    $('#cancel-button').click(function() {
+        window.location = `${data._metadata.root_url}/resources/committee`;
+    })
+    // Global post render
+    _post_render();
+
+    // Local post render
+
+    if (data._current_user && data._current_user.is_admin == true) {
+        $('.buttona').css('display', 'none')
+    }
+    addAuthenticityTokenToForms();
+
+};
